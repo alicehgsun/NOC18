3af7b30b0e25df5248958fe942a0c934a208d289
Alice Sun <hksun93@gmail.com>
2018-01-30 13:47:34 -0500

    window resize

--- LevyFlight/sketch.js
+++ LevyFlight/sketch.js
@@ -1,26 +1,21 @@
 
 var pos;
 var prev;
-var w;
-var h;
-
-w = 600;
-h = 600;
 
 function setup() {
-  createCanvas(w, h);
-background(0);
+  createCanvas(windowWidth, windowHeight);
+
   pos = createVector(mouseX, mouseY);
   prev = pos.copy();
   console.log(pos);
 }
 
 function draw() {
-  pos.x = constrain(pos.x, 0, w);
-  pos.y = constrain(pos.y, 0, h);
-
+  pos.x = constrain(pos.x, 0, windowWidth);
+  pos.y = constrain(pos.y, 0, windowHeight);
+  background(0,10);
   stroke(255);
-  strokeWeight(1);
+  strokeWeight(2);
   noFill();
   line(pos.x, pos.y, prev.x, prev.y);
   stroke(255);
@@ -30,9 +25,9 @@ function draw() {
 
   var r = random(100);
   if (r < 1) {
-    step.mult(random(50, 120));
+    step.mult(random(45, 100));
   } else {
-    step.setMag(20);
+    step.setMag(25);
   }
 
   //pos = pos + step;
@@ -42,11 +37,15 @@ function draw() {
   if (d>5){
     fill(random(255),random(255),random(255),50);
     noStroke();
-    triangle(pos.x, pos.y, prev.x, prev.y,w/2, h/2);
+    triangle(pos.x, pos.y, prev.x, prev.y,windowWidth/2, windowHeight/2);
   }
 
 }
 
 function mousePressed(){
   setup();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
